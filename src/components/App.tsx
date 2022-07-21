97927b8dd5124fc389ed00741ec0a679b90fb72b
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2022-07-22 01:03:59 +0900

    Add github icon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
 import { Typography } from "@mui/material";
 import { LoadFileAndWrite } from "./LoadFileAndWrite";
 import { SaveNewFile } from "./SaveNewFile";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const App = () => {
   return (
     <>
       <Typography variant="h3" component={"h1"} gutterBottom>
-        Text Editor Sample
+        Text Editor Sample{" "}
+        <a
+          href="https://github.com/SatoshiKawabata/file-system-access-api-sample"
+          target="_blank"
+          title="source code"
+        >
+          <GitHubIcon />
+        </a>
       </Typography>
       <SaveNewFile />
       <hr />
